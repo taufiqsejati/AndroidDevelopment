4623118a458a2a4323abbbb946c62edbbe8cb526
taufiq <taufiq@siklus.id>
2021-04-04 22:54:33 +0700

    Add Project Doctor App

---
+++ React Native/DoctorApp/src/config/Fire/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBuhjPvJ4GSHgs1da2Q3gBZ9DI7yYP5KIM',
+  authDomain: 'my-doctor-01-d6032.firebaseapp.com',
+  projectId: 'my-doctor-01-d6032',
+  storageBucket: 'my-doctor-01-d6032.appspot.com',
+  messagingSenderId: '889804805175',
+  appId: '1:889804805175:web:72e2527cda010962529bb2',
+  measurementId: 'G-3P7C0G9Z9E',
+});
+
+const Fire = firebase;
+
+export default Fire;
