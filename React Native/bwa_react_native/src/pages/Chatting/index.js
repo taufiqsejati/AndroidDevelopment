94c21f338138ba8451de74d4b2b01c5d082ddc77
Muhammad Taufiq <taufiq@siklus.id>
2021-04-20 15:22:57 +0700

    update

--- React Native/bwa_react_native/src/pages/Chatting/index.js
+++ React Native/bwa_react_native/src/pages/Chatting/index.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, View, StyleSheet, ScrollView} from 'react-native';
 import {ChatItem, Header, InputChat} from '../../components';
-import {colors, fonts} from '../../utils';
-
+import {colors, fonts, getData, showError} from '../../utils';
+import {Fire} from '../../config';
 const Chatting = ({navigation, route}) => {
   const dataDoctor = route.params;
+  const [chatContent, setChatContent] = useState('');
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    getData('user').then((res) => {
+      console.log('user login: ', res);
+      setUser(res);
+    });
+  }, []);
+
+  const chatSend = () => {
+    const today = new Date();
+    const hour = today.getHours();
+    const minutes = today.getMinutes();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const date = today.getDate();
+    const data = {
+      sendBy: user.uid,
+      chatDate: new Date().getTime(),
+      chatTime: `${hour}:${minutes} ${hour > 12 ? 'PM' : 'AM'}`,
+      chatContent: chatContent,
+    };
+    console.log('data untuk di kirim: ', data);
+    setChatContent('');
+
+    Fire.database()
+      .ref(
+        `chatting/${user.uid}_${dataDoctor.data.uid}/allChat/${year}-${month}-${date}`,
+      )
+      .push(data)
+      .then((res) => {
+        setChatContent('');
+      })
+      .catch((err) => {
+        showError(err.message);
+      });
+  };
   console.log('wew', dataDoctor.data.fullName);
   return (
     <View style={styles.page}>
@@ -24,9 +61,9 @@ const Chatting = ({navigation, route}) => {
         </ScrollView>
       </View>
       <InputChat
-        onChangeText={() => alert('inpu tap')}
-        value=""
-        onButtonPress={() => alert('button press')}
+        onChangeText={(value) => setChatContent(value)}
+        value={chatContent}
+        onButtonPress={chatSend}
       />
     </View>
   );
